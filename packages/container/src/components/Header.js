2fde7bd14fdcdced071580eeb23d7d544a1cc63d
kolinovsky <kolinovsky@gmail.com>
2023-04-14 16:01:02 +0200

    ch container textik

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -79,7 +79,7 @@ export default function Header({ isSignedIn, onSignOut }) {
             component={RouterLink}
             to="/"
           >
-            App
+            Ap p
           </Typography>
           <Button
             color="primary"
